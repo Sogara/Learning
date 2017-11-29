df2013badd5d7253144c823d00393da726d3f76e
user <sogara777@gmail.com>
2017-11-29 16:34:20 +0300

    commit

--- src/main/java/testCalculator/CalculatorTest.java
+++ src/main/java/testCalculator/CalculatorTest.java
@@ -11,6 +11,8 @@ public class CalculatorTest {
 
         ReadClass readClass = new ReadClass();  //вызов конструктора ReadClass
 
+        cycle:while (true){ // запуск бесконечного цикла, пока не нажата цифра (5 - Выход)
+
         double one; // обьявление типа и переменной
         double two;
 
@@ -31,17 +33,27 @@ public class CalculatorTest {
 
         int action = (int) readClass.readNextDouble(); // считывает значение в double и переводит его в int (1.0 в 1)
 
-        switch (action){ // вызывает и выводит в консоль выбранное действие: (add,subs,multi,div)
+        switch (action) { // вызывает и выводит в консоль выбранное действие: (add,subs,multi,div)
 
-            case 1: System.out.println(String.format("%.4f",calculator.add())); break;
-            case 2: System.out.println(String.format("%.4f",calculator.subs())); break;
-            case 3: System.out.println(String.format("%.4f",calculator.multi())); break;
-            case 4: System.out.println(String.format("%.4f",calculator.div())); break;
-            case 0: break;
+            case 1:
+                System.out.println(String.format("%.4f", calculator.add()));
+                break;
+            case 2:
+                System.out.println(String.format("%.4f", calculator.subs()));
+                break;
+            case 3:
+                System.out.println(String.format("%.4f", calculator.multi()));
+                break;
+            case 4:
+                System.out.println(String.format("%.4f", calculator.div()));
+                break;
+            case 0:
+                break ;
+            case 5: break cycle; // условия выхода из цыкла (нажатия цифры 5)
+            default:
+                System.out.println("Вы выбрали неверный пункт."); // вызывается, если введеное значение не соответсвует условию swith
 
-            default: System.out.println("Вы выбрали неверный пункт."); // вызывается, если введеное значение не соответсвует условию swith
-
-
+        }
 
         }
 
